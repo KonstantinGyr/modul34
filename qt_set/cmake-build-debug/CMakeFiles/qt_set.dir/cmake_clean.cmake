7e611258c6779f1b2ff0688c135d7c2f8590e743
KonstantinGyr <gyranovski@mail.ru>
2022-06-20 14:58:47 +0400

    'initial'

---
+++ qt_set/cmake-build-debug/CMakeFiles/qt_set.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/qt_set.dir/main.cpp.obj"
+  "libqt_set.dll.a"
+  "qt_set.exe"
+  "qt_set.exe.manifest"
+  "qt_set.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/qt_set.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
