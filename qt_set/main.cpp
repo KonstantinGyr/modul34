0d3abc5e92e7d6e98162b24d465de17e5d23738d
KonstantinGyr <gyranovski@mail.ru>
2022-07-03 16:06:21 +0400

    edition2

--- qt_set/main.cpp
+++ qt_set/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <QApplication>
+#include <QTimer>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+class Printer : public QObject{
+Q_OBJECT
+public slots:
+    void static printFinished(){
+        std::cout<<"finish"<<std::endl;
+    }
+};
+
+int main(int argc,char **argv) {
+    QApplication app(argc,argv);
+    QTimer myTimer;
+    Printer myPrinter;
+    //
+    QObject::connect(&myTimer,&QTimer::timeout ,&myPrinter,&Printer::printFinished);
+    myTimer.start(2000);
+    app.exec();
 }
+
